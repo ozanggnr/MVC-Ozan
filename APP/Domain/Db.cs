14e3e227fbdb806e708bf1d14093afd60c13c198
ozang <ozanggnr@gmail.com>
2025-10-25 12:57:23 +0300

    First Commit

---
+++ APP/Domain/Db.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Domain
+{
+    public class Db : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+
+        public Db(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ APP/Domain/Student.cs
@@ -0,0 +1,26 @@
+﻿using CORE.APP.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APP.Domain
+{
+    public class Student:Entity
+    {
+
+        [Required,StringLength(50)]
+        public string Name{ get; set; }
+
+        [Required, StringLength(50)]
+        public string Surname { get; set; }
+
+        public DateTime BirtDate { get; set; }
+
+        public decimal? OverallGrade { get; set; }
+
+        public bool IsGraduated { get; set; }
+    }
+}
