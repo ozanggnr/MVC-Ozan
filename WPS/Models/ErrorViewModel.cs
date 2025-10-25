271aa06f918664eb043a0ca41c244d60a0eb94ae
ozang <ozanggnr@gmail.com>
2025-10-25 11:27:43 +0300

    Add project files.

---
+++ WPS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WPS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
